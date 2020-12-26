3d2f6565d4fad16f69c9bb4fb009d0ed55d50608
madelinemc <madeline.coons@gmail.com>
2020-12-26 11:46:23 -0600

    created seed file for category table, removed extra category model and migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+
+d = Category.create(title: "Decor")
+f = Category.create(title: "Furniture")
+a = Category.create(title: "Architectural & Salvage")
+g = Category.create(title: "Garden")
+l = Category.create(title: "Lighting")
+app = Category.create(title: "Art, Posters & Paintings")
+s = Category.create(title: "Sculpture")
+c = Category.create(title: "Ceramics")
+kt = Category.create(title: "Kitchen & Tabletop")
+tx = Category.create(title: "Textiles")
+ca = Category.create(title: "Clothing & Accessories")
+b = Category.create(title: "Books")
+t = Category.create(title: "Toys")
+cu = Category.create(title: "Curiosities")
