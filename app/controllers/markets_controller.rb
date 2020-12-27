65e7c42976da942982d2b1061e95f301f763ca9d
madelinemc <madeline.coons@gmail.com>
2020-12-27 16:16:39 -0600

    added market params private method

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -2,9 +2,11 @@ class MarketsController < ApplicationController
     before_action :set_market, only: [:show, :edit, :update, :destory]
       
     def index
+        @markets = Market.all
     end
 
     def new
+        @market = Market.new
     end
 
     def create
@@ -26,6 +28,10 @@ class MarketsController < ApplicationController
 
     def set_market
         @market = Market.find(params[:id])
+    end
+
+    def market_params
+        params.require(:market).permit(:name, :location, :type, :season, :number_of_vendors)
     end
 
 end
