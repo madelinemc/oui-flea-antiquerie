fa29a82ab3e70db979679d096b70da039f31ee7b
madelinemc <madeline.coons@gmail.com>
2020-12-27 16:32:14 -0600

    defined antique routes - index, new, create, update, destroy and wrote antique params private method

--- app/controllers/antiques_controller.rb
+++ app/controllers/antiques_controller.rb
@@ -2,12 +2,16 @@ class AntiquesController < ApplicationController
     before_action :set_antique, only: [:show, :edit, :update, :destory]
 
     def index
+        @antiques = Antique.all
     end
 
     def new
+        @antique = Antique.new
     end
 
     def create
+        antique = Antique.create(antique_params)
+        redirect_to antique_path(antique)
     end
 
     def show
@@ -17,15 +21,25 @@ class AntiquesController < ApplicationController
     end
     
     def update
+        antique = Antique.find_by(id: params[:id])
+        antique.update(antique_params)
+        redirect_to antique_path(antique)
     end
 
     def destroy
+        antique = Antique.find_by(id: params[:id])
+        antique.destory
+        redirect_to antiques_path
     end
 
     private
 
     def set_antique
         @antique = Antique.find(params[:id])
+    end
+
+    def antique_params
+        params.require(:antique).permit(:description, :photo, :period, :provenance)
     end
 
 end
