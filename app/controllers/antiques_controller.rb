0cb70f9c347c9419bedbc25bc59d60b9d9c12768
madelinemc <madeline.coons@gmail.com>
2020-12-27 16:03:38 -0600

    added before action to find antique by id

--- app/controllers/antiques_controller.rb
+++ app/controllers/antiques_controller.rb
@@ -1,5 +1,6 @@
 class AntiquesController < ApplicationController
-    
+    before_action :set_antique, only: [:show, :edit, :update, :destory]
+
     def index
     end
 
@@ -19,6 +20,12 @@ class AntiquesController < ApplicationController
     end
 
     def destroy
+    end
+
+    private
+
+    def set_antique
+        @antique = Antique.find(params[:id])
     end
 
 end
