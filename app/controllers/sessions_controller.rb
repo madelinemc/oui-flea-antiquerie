4900aad7ae3737f297771a54328fc6d18b0f413e
madelinemc <madeline.coons@gmail.com>
2020-12-29 14:38:47 -0600

    added log in and out actions to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
-    def index
+    before_action :already_logged_in?, only: [:home, :new]
+
+    def home #main page of application
     end
 
     def new
@@ -7,15 +9,29 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.create
+        @user = User.find_by(username: params[:user][:username])
+        if @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render new_session_path
+        end
     end
 
     def destory
+        session.clear
+        redirect_to home_path
     end
 
     def omniauth_login
     end
 
     private
+
+    def already_logged_in?
+        if is_logged_in?
+            redirect_to user_path(User.find_by_id(session[:user_id])
+        end
+    end
 
 end
