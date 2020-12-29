2e496fea56ecbab7dd0a938965c206ec5e2742b2
madelinemc <madeline.coons@gmail.com>
2020-12-29 12:09:11 -0600

    updated routes and added views for markets - index, show, edit, new plus updated antiques edit and new forms

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:index]
 
-  get '/antiques', to: 'antiques#index'
-  get '/antiques/new', to: 'antiques#new'
-  get '/antiques/:id', to: 'antiques#show'
-  post '/antiques', to: 'antiques#create'
-  get '/antiques/:id/edit', to: 'antiques#edit'
-  patch '/antiques/:id', to: 'antiques#update'
-  delete '/antiques/:id/delete', to: 'antiques#destroy'
+  # get '/antiques', to: 'antiques#index'
+  # get '/antiques/new', to: 'antiques#new'
+  # get '/antiques/:id', to: 'antiques#show'
+  # post '/antiques', to: 'antiques#create'
+  # get '/antiques/:id/edit', to: 'antiques#edit'
+  # patch '/antiques/:id', to: 'antiques#update'
+  # delete '/antiques/:id/delete', to: 'antiques#destroy'
 
   resources :markets
+  resources :antiques
 
 end
