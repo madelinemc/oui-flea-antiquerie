cf60b174d6fcb803e7723081a290b4e23b6f305f
madelinemc <madeline.coons@gmail.com>
2021-01-02 18:16:44 -0600

    added helper method to render market image in views

--- app/helpers/markets_helper.rb
+++ app/helpers/markets_helper.rb
@@ -1,2 +1,10 @@
 module MarketsHelper
+
+    def select_market_image(market)
+        if market.market_photo.attachment
+            "<img> #{image_tag @market.market_photo} </img>".html_safe
+        else
+            "<img> #{image_tag("market-placeholder-image")} </img>".html_safe
+        end
+    end
 end
