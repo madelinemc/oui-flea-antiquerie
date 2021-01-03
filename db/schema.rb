bde7e2934576753442e34b63bd3405422501bfbd
madelinemc <madeline.coons@gmail.com>
2021-01-03 12:23:58 -0600

    updated users table with uid and provider columns and added oauth gems to gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_31_204710) do
+ActiveRecord::Schema.define(version: 2021_01_03_181905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,8 @@ ActiveRecord::Schema.define(version: 2020_12_31_204710) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.string "email"
+    t.string "provider"
+    t.string "uid"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
