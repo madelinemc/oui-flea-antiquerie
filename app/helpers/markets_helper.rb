bbce51c1e92bd17b2ddd908ccbfc6add8ebcbab8
madelinemc <madeline.coons@gmail.com>
2021-01-03 20:12:51 -0600

    update markets view layout

--- app/helpers/markets_helper.rb
+++ app/helpers/markets_helper.rb
@@ -1,8 +1,8 @@
 module MarketsHelper
 
-    def select_market_image(market)
+    def select_market_image(market, is_instance_variable)
         if market.market_photo.attachment
-            "<img> #{image_tag @market.market_photo} </img>".html_safe
+            is_instance_variable ? "<img> #{image_tag @market.market_photo} </img>".html_safe : "<img> #{image_tag market.market_photo} </img>".html_safe
         else
             "<img> #{image_tag("market-placeholder-image")} </img>".html_safe
         end
